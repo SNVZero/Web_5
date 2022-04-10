660161ee6173005a3e946d450c6f60f56d8c0953
SNVZero <simakov_nikita01@mail.ru>
2022-04-10 20:42:29 +0300

    Logout

--- login.php
+++ login.php
@@ -10,12 +10,12 @@ $check_user = mysqli_quety($connection, "SELECT * FROM USERS WHERE login = '$log
 if(mysqli_num_rows($check_user) > 0){
     $user = mysqli_fetch_assoc($check_user);
     $_SESSION['user'] = [
-        "name" => $user['name'];
-        "email" => $user['mail'];
-        "bio" => $user['bio'];
-        "year" => $user['date'];
-        "gender" => $user['gender'];
-        "limbs" => $user['limbs'];
+        "name" => $user['name'],
+        "email" => $user['mail'],
+        "bio" => $user['bio'],
+        "year" => $user['date'],
+        "gender" => $user['gender'],
+        "limbs" => $user['limbs']
     ];
     header('Location: index.php');
 }else{
