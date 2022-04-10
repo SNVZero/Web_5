8e73be52b510f3ee42782fbf0a8e402f05753bc2
SNVZero <simakov_nikita01@mail.ru>
2022-04-10 18:46:14 +0300

    Logout

--- login.php
+++ login.php
@@ -22,42 +22,49 @@ if(isset($_POST['do_login'])){
     }
 }
 ?>
+<!DOCTYPE html>
+<html>
    <head>
         <meta charset="utf-8">
         <link rel="stylesheet" href="css/style.css">
         <title>Вход</title>
 </head>
-<form  method="post" action="popuplogin.php">
-    <div class="popup" id="popup">
-        <div class="popup__body">
-            <div class="popup__content">
-                <div class="popup__title">Вход</div>
-                <div class="popup__text">
+<body>
+
+
+    <form  method="post" action="popuplogin.php">
+        <div class="popup" id="popup">
+            <div class="popup__body">
+                <div class="popup__content">
+                    <div class="popup__title">Вход</div>
+                    <div class="popup__text">
+                            <div>
+                                <input type="text" name="login" class="login__elem" placeholder="Логин" value="<?php @$_POST['login'] ?>">
+                                <div class="text-danger err ">
+                                            <?php
+                                                if(@$mess['login'] == TRUE){
+                                                    print('Неправильно введен логин');
+                                                }
+                                            ?>
+                                        </div>
+                            </div>
+                            <div>
+                                <input type="text" name="password" class="login__elem" placeholder="Пароль">
+                                <div class="text-danger err ">
+                                            <?php
+                                                if(@$mess['pass'] == TRUE){
+                                                    print('Неправильно введен пароль');
+                                                }
+                                            ?>
+                                        </div>
+                            </div>
                         <div>
-                            <input type="text" name="login" class="login__elem" placeholder="Логин" value="<?php @$_POST['login'] ?>">
-                            <div class="text-danger err ">
-                                        <?php
-                                            if(@$mess['login'] == TRUE){
-                                                print('Неправильно введен логин');
-                                            }
-                                        ?>
-                                    </div>
+                            <input type="submit" class="popup__btn" value="Войти" name="do_login">
                         </div>
-                        <div>
-                            <input type="text" name="password" class="login__elem" placeholder="Пароль">
-                            <div class="text-danger err ">
-                                        <?php
-                                            if(@$mess['pass'] == TRUE){
-                                                print('Неправильно введен пароль');
-                                            }
-                                        ?>
-                                    </div>
-                        </div>
-                    <div>
-                        <input type="submit" class="popup__btn" value="Войти" name="do_login">
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</form>
+    </form>
+</body>
+</html>
