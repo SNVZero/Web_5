14c965566fce44df8523d34e1c2d337c5dd0571f
SNVZero <simakov_nikita01@mail.ru>
2022-04-10 18:08:27 +0300

    Logout

--- popuplogin.php
+++ popuplogin.php
@@ -3,14 +3,14 @@ require 'db.php';
 if(isset($_POST['do_login'])){
     $mess = array();
     $stmt = $db->prepare("SELECT login FROM USERS WHERE login = ?" );
-    $stmt->execute($_POST['login']);
+    $stmt->execute([$_POST['login']]);
     $login = $stmt->fetch(PDO::FETCH_LAZY);
     if($login != $_POST['login']){
         $mass['login']= TRUE;
     }else{
         $mess['login'] = FALSE;
         $stmt = $db->prepare("SELECT pass FROM USERS WHERE login = ?" );
-        $stmt->execute($_POST['login']);
+        $stmt->execute([$_POST['login']]);
         $password = $stmt->fetch(PDO::FETCH_LAZY);
         if(password_verify($_POST['password'],$password)){
             $mess['password']= FALSE;
@@ -22,7 +22,7 @@ if(isset($_POST['do_login'])){
     }
 }
 ?>
-<form  method="post" action="popuplogin.php">
+<form  method="post" action="">
     <div class="popup" id="popup">
         <div class="popup__body">
             <div class="popup__content">
